556be6a328f2f0c19f442564b0ed8b7c29f9e2de
Nguyen Trung Kien <nguyentrungkien17102004@gmail.com>
2025-05-27 23:20:20 +0700

    fix cicd

--- src/app/apis/profile.api.ts
+++ src/app/apis/profile.api.ts
@@ -31,7 +31,7 @@ export const profileApi = {
       })
       return response.data
     } catch (error) {
-      console.error('Failed to update profi:', error)
+      console.error('Failed to update profile:', error)
       throw error
     }
   }
