47b0953c8d14c2adb524f5893cf2e60bc67b2db6
Nguyen Trung Kien <nguyentrungkien17102004@gmail.com>
2025-06-03 18:45:10 +0700

    fix: payment

--- src/app/apis/fetcher.ts
+++ src/app/apis/fetcher.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const fetcher = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URLL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
